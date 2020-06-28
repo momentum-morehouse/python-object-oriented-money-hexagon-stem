3e32443b3788fdb5dd32c6b902842fd43580e4e6
hexagonstem <bishara@hexagon-stem.com>
2020-06-28 01:34:02 +0000

    create Currency and Money classes that pass all tests

--- money.py
+++ money.py
@@ -27,20 +27,21 @@ class Currency:
         """
         Should return the currency code, or code with symbol in parentheses.
         """
-        return '{} {}' .format(self.name,self.digits)
-        return '{} {}' .format(self.symbol,self.code)
-        #return f"{self.name} of {self.digits}" 
-        #return f"{self.symbol} of {self.code}"
+        if self.symbol:  #add what varies to the conditional
+            return f"{self.code} ({self.symbol})"
+        else:
+            return f"{self.code}"
 
-
-    def __eq__(self, other): 
+    def __eq__(self, other):
         """
         All fields must be equal to for the objects to be equal.
         """
-        return (type(self) == type(other) and self.name == other.name and
-                self.code == other.code and self.symbol == other.symbol and
-                self.digits == other.digits)
+        return (type(self) == type(other) and self.name == other.name
+                and self.code == other.code and self.symbol == other.symbol
+                and self.digits == other.digits)
 
+
+#how information is stored in database
 class Money:
     """
     Represents an amount of money. Requires an amount and a currency.
@@ -52,88 +53,76 @@ class Money:
         - amount -- quantity of currency
         - currency -- type of currency
         """
-        
+
         self.amount = amount
         self.currency = currency
-        
-       
-        
+
+    #Make it human readable as a string
     def __str__(self):
         """
         Should use the currency symbol if available, else use the code.
         Use the currency digits to determine number of digits to show.
         """
         if self.currency.symbol:
-            return f"{self.currency.symbol}{self.amount:}{self.currency.digits}"
+            return f"{self.currency.symbol}{self.amount:.{self.currency.digits}f}"
         else:
-            return f"{self.currency.code}{self.amount:}{self.currency.digits}"
+            return f"{self.currency.code} {self.amount:.{self.currency.digits}f}"
 
+
+#f string syntax- limited them based on digits
 
     def __repr__(self):
-        return f"<Money {str(self.currency), str(self.amount)}>"
-
-#def __repr__(self):
-        #return {'name':self.name, 'age':self.age}
-
-    
-
+        return f"<Money {str(self)}>"
 
     def __eq__(self, other):
         """
         All fields must be equal to for the objects to be equal.
         """
 
-        return (type(self) == type(other) and self.amount == other.amount and
-                self.currency == other.currency)
+        return (type(self) == type(other) and self.amount == other.amount
+                and self.currency == other.currency)
 
     def add(self, other):
         """
         Add two money objects of the same currency. If they have different
         currencies, raise a DifferentCurrencyError.
         """
-    
         if self.currency == other.currency:
-            return
-            amount = self.amount + other.amount,
+            new_amount = self.amount + other.amount
             currency = self.currency
-
-        raise TypeError('DifferentCurrencyError')
-
+            sum = Money(new_amount, currency)  #create a new instance of money
+            return sum
+        else:
+            raise DifferentCurrencyError
 
     def sub(self, other):
         """
         Subtract two money objects of the same currency. If they have different
         currencies, raise a DifferentCurrencyError.
         """
-
         if self.currency == other.currency:
-            return
-            amount = self.amount - other.amount,
+            new_amount = self.amount - other.amount
             currency = self.currency
-
-        raise TypeError('DifferentCurrencyError')
-
+            difference = Money(new_amount,
+                               currency)  #create a new instance of money
+            return difference
+        else:
+            raise DifferentCurrencyError
 
     def mul(self, multiplier):
         """
         Multiply a money object by a number to get a new money object.
         """
-        if self.currency == other.currency:
-            return
-            amount = self.amount * other.amount,
-            currency = self.currency
-
-        raise TypeError('DifferentCurrencyError')
-
-
+        new_amount = self.amount * multiplier
+        currency = self.currency
+        product = Money(new_amount, currency)  #create a new instance of money
+        return product
 
     def div(self, divisor):
         """
         Divide a money object by a number to get a new money object.
         """
-        if self.currency == other.currency:
-            return
-            amount = self.amount / other.amount,
-            currency = self.currency
-
-        raise TypeError('DifferentCurrencyError')
+        new_amount = self.amount / divisor
+        currency = self.currency
+        quotient = Money(new_amount, currency)  #create a new instance of money
+        return quotient
